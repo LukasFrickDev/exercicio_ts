ac5c54fd2bc58b4bf6e33cc98a3ba186a897ac72
Lukas Frick <contatolukasfrick@gmail.com>
2024-11-22 15:13:28 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+function mutiplicacaoNumeros(numeroUm, numeroDois) {
+    return numeroUm * numeroDois;
+}
+console.log(mutiplicacaoNumeros(20, 30));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+const Nome = 'Lukas';
+console.log(saudacao(Nome));
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+function mutiplicacaoNumeros(numeroUm:number, numeroDois:number):number {
+    return numeroUm * numeroDois
+}
+
+console.log(mutiplicacaoNumeros(20,30))
+
+function saudacao(nome:string):string {
+    return `Olá ${nome}`;
+}
+
+const Nome = 'Lukas'
+console.log(saudacao(Nome))
